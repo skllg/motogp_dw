abbbf441c5a09e7d791f2ea4ea2afea07a686294
Zakaria Smaty <zakaria.smaty@stratebi.com>
2024-01-31 10:25:31 +0100

    mejorado el script hastade 2012-2023

--- Scripts Python/motogp/motogp/spiders/spider_wiki.py
+++ Scripts Python/motogp/motogp/spiders/spider_wiki.py
@@ -3,26 +3,37 @@ import scrapy
 
 class WikipediaSpider(scrapy.Spider):
     name = 'wikipedia_spider'
-    season = 2023
-    start_urls = [f'https://en.wikipedia.org/wiki/{season}_MotoGP_World_Championship#Teams_and_riders']
+    # https://en.wikipedia.org/wiki/2003_Grand_Prix_motorcycle_racing_season
 
-    def parse(self, response):
+    def start_requests(self):
+        for season in range(2012,2024):
+                            
+            start_urls = f'https://en.wikipedia.org/wiki/{season}_MotoGP_World_Championship#Teams_and_riders'
+            yield scrapy.Request(url=str(start_urls),cb_kwargs={'season':season}, callback=self.parse)
+        # for season in range(2002,2012):
+        #     start_urls = f'https://en.wikipedia.org/wiki/{season}_Grand_Prix_motorcycle_racing_season#Participants'
+        #     yield scrapy.Request(url=str(start_urls),cb_kwargs={'season':season}, callback=self.parse)
+    def parse(self, response,season):
         # Locate the table using XPath
-        table_rows = response.xpath('//tr[td[contains(@style, "background:#ccffcc;")]]')
+        table_rows = response.xpath('//tr[td[contains(@style, "background:#ccffcc;") or contains(@style, "background:#cfc;")]]')
 
         for row in table_rows:
-            background_color = row.xpath('.//td[contains(@style, "background:#ccffcc;")]/@style').extract_first()
-            title = row.xpath('.//td[contains(@style, "background:#ccffcc;")]//a/@title').extract()
-            last_td = row.xpath('.//td[contains(@style, "background:#ccffcc;")]/following-sibling::td[last()]/text()').extract_first()
+            background_color = row.xpath('.//td[contains(@style, "background:#ccffcc;") or contains(@style, "background:#cfc;")]/@style').extract_first()
+            title = row.xpath('.//td[contains(@style, "background:#ccffcc;") or contains(@style, "background:#cfc;")]//a/@title').extract()
+            # last_td = row.xpath('.//td[contains(@style, "background:#ccffcc;")]/following-sibling::td[last()]/text()').extract_first()
+            last_td = row.xpath('.//td[contains(@style, "background:#ccffcc;") or contains(@style, "background:#cfc;")]/following-sibling::td[last()]').extract_first()
+
+            last_td = ''.join(scrapy.Selector(text=last_td).xpath('//text()').extract()).strip()
 
             # Remove leading/trailing whitespaces
             title_proc = str(title).split(",")
 
             last_td = last_td.strip() if last_td else ''
 
-            # Append the results to a CSV file
+            # Append the results to a CSV file 'rider_name': title_proc[1].replace(']', ''),
             yield {
-                'Title': f'Wildcards_{2023}',
+                'Title': f'Wildcards',
+                'season': season,
                 'rider_name': title_proc[1].replace(']', ''),
                 'rounds_wildcard': str(last_td),
             }
